18e73dd1da2176765a97636fac01ed994c124ed1
AyushiBadika <badikaayushi@gmail.com>
2024-05-16 16:58:01 +0530

    updated result files

--- result/result.js
+++ result/result.js
@@ -5,6 +5,8 @@ const wikipediaResults = document.querySelector(".wikipediaResults");
 const googleResults = document.querySelector(".googleResults");
 const geminiResults = document.querySelector(".geminiResults");
 const searchHistory = document.querySelector(".searchHistory");
+const dropdown = document.querySelector(".sortDropdown");
+let wikipediaOutput = [];
 
 // loaders
 const googleLoader = document.querySelector(".googleLoader");
@@ -45,7 +47,8 @@ async function fetchWikipediaResults(query) {
     wikipediaResults.classList.add("noResults");
   } else {
     wikipediaResults.classList.remove("noResults");
-    data.query.search.forEach((result) => {
+    wikipediaOutput = data.query.search;
+    wikipediaOutput.forEach((result) => {
       createWikipediaCard(
         result.title,
         `https://en.wikipedia.org/wiki/${result.title.replace(/ /g, "_")}`,
@@ -73,7 +76,7 @@ function storeQueryInLocalStorage(query) {
   let history = localStorage.getItem("searchHistory");
   if (history != null) {
     history = history.split(",");
-    let noDuplicate = new Set(history).add(query);
+    let noDuplicate = new Set(history).add(query.toString().toLowerCase());
     history = [];
     noDuplicate.forEach((value) => {
       history.push(value);
@@ -159,6 +162,7 @@ document.querySelector(".googleTab").addEventListener("click", () => {
   document.querySelector(".googleSection").style.display = "flex";
   document.querySelector(".wikipediaSection").style.display = "none";
   document.querySelector(".geminiSection").style.display = "none";
+  dropdown.style.display = "block";
 });
 document.querySelector(".wikipediaTab").addEventListener("click", () => {
   document.querySelector(".googleTab").classList.remove("selectedTab");
@@ -168,6 +172,7 @@ document.querySelector(".wikipediaTab").addEventListener("click", () => {
   document.querySelector(".googleSection").style.display = "none";
   document.querySelector(".wikipediaSection").style.display = "flex";
   document.querySelector(".geminiSection").style.display = "none";
+  dropdown.style.display = "block";
 });
 document.querySelector(".geminiTab").addEventListener("click", () => {
   document.querySelector(".googleTab").classList.remove("selectedTab");
@@ -177,6 +182,7 @@ document.querySelector(".geminiTab").addEventListener("click", () => {
   document.querySelector(".googleSection").style.display = "none";
   document.querySelector(".wikipediaSection").style.display = "none";
   document.querySelector(".geminiSection").style.display = "flex";
+  dropdown.style.display = "none";
 });
 
 //voice to text
@@ -215,6 +221,7 @@ mic.addEventListener("click", function () {
 
 searchIcon.addEventListener("click", (e) => {
   if (searchBarInput.value == "") return;
+  searchBarInput.blur();
   wikipediaLoader.style.display = "block";
   googleLoader.style.display = "block";
   geminiLoader.style.display = "block";
@@ -235,6 +242,7 @@ searchBarInput.addEventListener("keydown", (e) => {
   if (e.key == "Enter" || e.key == 13) {
     if (searchBarInput.value == "") return;
     e.preventDefault();
+    searchBarInput.blur();
     wikipediaLoader.style.display = "block";
     googleLoader.style.display = "block";
     geminiLoader.style.display = "block";
@@ -251,186 +259,6 @@ searchBarInput.addEventListener("keydown", (e) => {
   }
 });
 
-const obj = {
-  search_term: "football",
-  knowledge_panel: {
-    name: "NFL",
-    label: "League",
-    description: {
-      text: "The National Football League is a professional American football league that consists of 32 teams, divided equally between the American Football Conference and the National Football Conference.",
-      url: "https://en.wikipedia.org/wiki/National_Football_League",
-      site: "Wikipedia",
-    },
-    image: {
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:-HmtpTOH5MCDmM",
-      width: 179,
-      height: 246,
-      page_url: "https://en.wikipedia.org/wiki/National_Football_League",
-    },
-    info: [
-      {
-        title: "Commissioner",
-        labels: ["Roger Goodell"],
-      },
-      {
-        title: "First season",
-        labels: ["1920"],
-      },
-      {
-        title: "Founded",
-        labels: ["September 17, 1920; 103 years ago; Canton, Ohio, U.S."],
-      },
-      {
-        title: "Most titles",
-        labels: ["Green Bay Packers; (13 titles)"],
-      },
-      {
-        title: "Sport",
-        labels: ["American football"],
-      },
-    ],
-  },
-  results: [
-    {
-      position: 1,
-      url: "https://www.nfl.com/",
-      title: "NFL.com | Official Site of the National Football League",
-      description:
-        "The official source for NFL News, NFL video highlights, Fantasy Football, game-day coverage, NFL schedules, stats, scores & more.",
-    },
-    {
-      position: 2,
-      url: "https://en.wikipedia.org/wiki/Football",
-      title: "Football - Wikipedia",
-      description:
-        "Football is a family of team sports that involve, to varying degrees, kicking a ball to score a goal. Unqualified, the word football normally means the form ...",
-    },
-    {
-      position: 3,
-      url: "https://www.aljazeera.com/sports/liveblog/2024/5/12/live-manchester-united-vs-arsenal-english-premier-league-football",
-      title:
-        "LIVE: Manchester United vs Arsenal – English Premier League football",
-      description:
-        "Arsenal beat Manchester United 1-0 to move back to the top of the Premier League. Leandro Trossard gave the Gunners the lead after 20 ...",
-    },
-    {
-      position: 4,
-      url: "https://usafootball.com/",
-      title: "USA Football | The Sport's Governing Body",
-      description:
-        "USA Football's here to lead, strengthen and grow the game alongside you as the sport's governing body through education, events and the U.S. National Team ...",
-    },
-    {
-      position: 5,
-      url: "https://en.wikipedia.org/wiki/American_football",
-      title: "American football - Wikipedia",
-      description:
-        "American football also known as gridiron football, is a team sport played by two teams of eleven players on a rectangular field with goalposts at each end.",
-    },
-    {
-      position: 6,
-      url: "https://www.espn.com/nfl/",
-      title: "NFL on ESPN - Scores, Stats and Highlights",
-      description:
-        "Visit ESPN for NFL live scores, video highlights and latest news. Stream Monday Night Football on ESPN+ and play Fantasy Football.",
-    },
-    {
-      position: 7,
-      url: "https://www.espn.com/college-football/",
-      title: "NCAA on ESPN - College Football Scores, Stats and Highlights",
-      description:
-        "Visit ESPN for NCAA live scores, video highlights and latest news. Stream exclusive college football games on ESPN+ and play College Pick'em.",
-    },
-    {
-      position: 8,
-      url: "https://www.youtube.com/watch?v=RCd2dxzOkg4",
-      title: "Football is the GREATEST team game there is   - YouTube",
-      description:
-        "MicdUp #KevinStefanski #BrownsRookieMinicamp SUBSCRIBE: https://www.youtube.com/c ...",
-    },
-    {
-      position: 9,
-      url: "https://www.britannica.com/sports/football-soccer",
-      title: "Football (soccer) | History, Game, Rules, & Significant Players",
-      description:
-        "Football, also called soccer, is a game in which two teams of 11 players, using any part of their bodies except their hands and arms, try to maneuver the ...",
-    },
-    {
-      position: 10,
-      url: "https://www.cbssports.com/nfl/",
-      title: "NFL Football - News, Scores, Stats, Standings, and Rumors",
-      description:
-        "CBS Sports has the latest NFL Football news, live scores, player stats, standings, fantasy games, and projections.",
-    },
-  ],
-  related_keywords: {
-    spelling_suggestion_html: null,
-    spelling_suggestion: null,
-    keywords: [
-      {
-        position: 1,
-        knowledge: null,
-        keyword_html: "football<b> games today</b>",
-        keyword: "football games today",
-      },
-      {
-        position: 2,
-        knowledge: null,
-        keyword_html: "football",
-        keyword: "football",
-      },
-      {
-        position: 3,
-        knowledge: null,
-        keyword_html: "football<b> today</b>",
-        keyword: "football today",
-      },
-      {
-        position: 4,
-        knowledge: null,
-        keyword_html: "football<b> games</b>",
-        keyword: "football games",
-      },
-      {
-        position: 5,
-        knowledge: null,
-        keyword_html: "football<b> schedule</b>",
-        keyword: "football schedule",
-      },
-      {
-        position: 6,
-        knowledge: null,
-        keyword_html: "football<b> scores</b>",
-        keyword: "football scores",
-      },
-      {
-        position: 7,
-        knowledge: null,
-        keyword_html: "football<b> games this weekend</b>",
-        keyword: "football games this weekend",
-      },
-      {
-        position: 8,
-        knowledge: null,
-        keyword_html: "football<b> tonight</b>",
-        keyword: "football tonight",
-      },
-      {
-        position: 9,
-        knowledge: null,
-        keyword_html: "football<b> playoffs</b>",
-        keyword: "football playoffs",
-      },
-      {
-        position: 10,
-        knowledge: null,
-        keyword_html: "football<b> cleats</b>",
-        keyword: "football cleats",
-      },
-    ],
-  },
-};
-
 // GEMINI API
 const md = markdownit({
   html: true,
@@ -440,7 +268,13 @@ const md = markdownit({
 
 async function fetchGeminiResults(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-  const result = await model.generateContent(prompt);
+  const result = await model.generateContent(prompt).catch((err) => {
+    console.log("error in gemini fetch");
+    geminiResults.innerHTML =
+      "Error fetching gemini results, please reload or try again later!";
+    geminiLoader.style.display = "none";
+    geminiResults.style.display = "flex";
+  });
   const response = result.response;
   const text = response.text();
 
@@ -460,8 +294,11 @@ async function fetchGeminiResults(prompt) {
 }
 
 // Google Api
+
 async function fetchGoogleResults(query) {
-  const apiEndpoint = `https://www.googleapis.com/customsearch/v1?key=AIzaSyCrxkJNYYAFzWqYT3tL2g23lg0c1IIPy-U&cx=832c9a23ff2784c5a&q=${query}`;
+  const sort = dropdown.value == "date" ? "date" : "";
+
+  const apiEndpoint = `https://www.googleapis.com/customsearch/v1?q=${query}&num=10&cx=832c9a23ff2784c5a&sort=${sort}&key=AIzaSyCrxkJNYYAFzWqYT3tL2g23lg0c1IIPy-U`;
   const response = await fetch(apiEndpoint);
   const data = await response.json();
   console.log(data);
@@ -507,3 +344,40 @@ function createWGoogleCard(title, link, description, thumbnail, appName) {
   </div>
 </div>`;
 }
+
+// sort drop down
+dropdown.addEventListener("change", () => {
+  if (dropdown.value == "date") {
+    //sort wikipedia
+    let filteredResults = [...wikipediaOutput];
+    filteredResults.sort((a, b) => {
+      return new Date(b.timestamp) - new Date(a.timestamp);
+    });
+    wikipediaResults.innerHTML = "";
+    filteredResults.forEach((result) => {
+      createWikipediaCard(
+        result.title,
+        `https://en.wikipedia.org/wiki/${result.title.replace(/ /g, "_")}`,
+        result.timestamp.slice(0, 10),
+        result.snippet
+      );
+    });
+
+    //sort google
+    fetchGoogleResults(searchBarInput.value);
+  } else {
+    //sort wikipedia
+    wikipediaResults.innerHTML = "";
+    wikipediaOutput.forEach((result) => {
+      createWikipediaCard(
+        result.title,
+        `https://en.wikipedia.org/wiki/${result.title.replace(/ /g, "_")}`,
+        result.timestamp.slice(0, 10),
+        result.snippet
+      );
+    });
+
+    //sort google
+    fetchGoogleResults(searchBarInput.value);
+  }
+});
